6401c255e382a802fc36b014145f862836cba2f1
jstnliu <jlcheeseburger@gmail.com>
2023-09-21 14:10:48 -0700

    pseudocode

---
+++ minesweep.js
@@ -0,0 +1,51 @@
+//necessary constants 
+
+
+
+
+
+
+//defining variables
+
+
+
+
+
+
+
+
+//code that runs 'minesweeper' functions
+//initialize
+//generate grid (trying for difficulty selection ie. 10x10 = easy, 25x25 = med, etc.)
+//randomly place newly generated mines 
+
+
+
+
+
+//render
+//update board
+//check for win/loss/in-progress
+
+
+//create game spaces
+
+
+
+//make game spaces clickable
+
+
+    //click events
+//check for open space or bomb space
+//if open, clear surrounding spaces/list number of mines around 3x3
+
+
+//else reveal mines upon loss (hopefully)
+//display a "death" message 
+//turn start button to restart button
+
+
+
+
+
+//
