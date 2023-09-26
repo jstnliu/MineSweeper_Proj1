b500331a738e493ebc9a1b7a415804e9925177f2
jstnliu <jlcheeseburger@gmail.com>
2023-09-26 14:03:54 -0700

    new variables, createTiles improvements WIP, handleTiles WIP

--- js/minesweep.js
+++ js/minesweep.js
@@ -8,7 +8,8 @@ const THE_BUTTON = 'the-button';
 
     //State Variables
 let results;
-
+let totalBoomSpaces;
+let totalOpenSpaces;
 
     //Cached Element References
     //'you win' or 'you lose' element 
@@ -16,6 +17,11 @@ const messagesDisplay = document.getElementById(MESSAGES_DISPLAY);
 const gameBoard = document.getElementById(GAME_BOARD_ID);
     //start/replay button
 const theButton = document.getElementById(THE_BUTTON);
+    //Tile Spaces Variables
+const totalTiles = gridSize * gridSize;
+let gridSize = 10; 
+let bombAmount = 20;
+let tiles = [];
 
 
     //Click Events (Use Event Delegation -Jim Clark)
@@ -47,11 +53,11 @@ function createTileSpaces() {
     // div*5 --> makes 5 divs
     //generate grid (trying for difficulty selection ie. 10x10 = easy, 25x25 = med, etc.)
     //randomly place newly generated mines 
-   const gridSize = 10; 
-   const totalTiles = gridSize * gridSize;
    for (let i = 0; i < totalTiles; i++){
     const tile = createGameTiles();
+    tile.setAttribute('id', i);
     gameBoard.append(tile);
+    tiles.push(tile);
    }
 }
 
@@ -64,33 +70,52 @@ function createGameTiles() {
     tile.classList.add('tile');
     tile.style.backgroundColor = '#333';
     const randomNumber = Math.random() < 0.1;
-    let totalOpenSpaces = 0;
-    // let totalBoomSpaces = 0; // research a way make it like the TTT example
+    totalOpenSpaces = 0;
+    totalBoomSpaces = 0; // research a way make it like the TTT example
     if (randomNumber && totalOpenSpaces < 90) {
+        tile.classList.add('boom-space');
         tile.innerText = "BOOM";
-        tile.addEventListener('click', handleMineSpace)
+        totalBoomSpaces++;
+        tile.addEventListener('click', handleBoomSpace)
     } else {
+        tile.classList.add('open-space');
         totalOpenSpaces++;
-        
+        tile.addEventListener('click', handleOpenSpace)
     }
     return tile;
 }
 
 
-//function createMineSpaces
+//function createMineSpaces (Somewhat achieved in 'createGameTiles'?)
 //randomly placed 
 //up to a certain number (per difficulty)
 
-//handleOpenSpace(+)
+function handleBoomSpace(event) {
+    const boomSpaces = document.querySelectorAll('.boom-space');
+    event.target.style.backgroundColor = 'red'; 
+    // boomSpaces.forEach(event.target.removeEventListener('click', handleBoomSpace));
+    messagesDisplay.innerText = 'BOOM!'
+    theButton.innerText = 'Try Again!';
+    // boomSpaces.style.backgroundColor = 'red';
+}
+
+function handleOpenSpace(event) {
     //click on space to 'reveal' 
     //check for open space or bomb space
     //if open, clear surrounding spaces/list number of mines around 3x3
     //use ternary operator to count bombs
+    // totalOpenSpaces.style.backgroundColor = lightgrey;
+    const openSpaces = document.querySelectorAll('.open-space');
+    event.target.style.backgroundColor = 'white';
 
-//handleMineSpace(-)
+}
+
+// function handleMineSpace() {
     //else reveal mines upon loss (hopefully)
     //display a "death" message 
     //turn start button to restart button
+    // totalBoomSpaces.style.backgroundColor = red;
+// }
 
 
 
