384bce7c9e66ba71dc9343e6ca949a68f04b4fac
jstnliu <jlcheeseburger@gmail.com>
2023-09-26 16:10:46 -0700

    rearranged pseudo for clarity

--- js/minesweep.js
+++ js/minesweep.js
@@ -50,43 +50,39 @@ function render() {
 
 function createTileSpaces() {
     //Arrays to store open and bomb spaces 
-    //randomly place newly generated mines 
-   for (let i = 0; i < totalTiles; i++){
-    const tile = createGameTiles();
-    tile.setAttribute('id', i);
-    gameBoard.append(tile);
-    tiles.push(tile);
-   }
+    for (let i = 0; i < totalTiles; i++){
+        const tile = createGameTiles();
+        tile.setAttribute('id', i);
+        gameBoard.append(tile);
+        tiles.push(tile);
+    }
 }
 
 
 function createGameTiles() {
     //create game spaces
-    //make game spaces clickable
-    //check for win/loss/in-progress
     const tile = document.createElement('div');
     tile.classList.add('tile');
     tile.style.backgroundColor = '#333';
+    //randomly place newly generated mines 
     const randomNumber = Math.random() < 0.2;
     totalOpenSpaces = 0;
     totalBoomSpaces = 0; // research a way make it like the TTT example
     if (randomNumber && totalOpenSpaces < 80) {
+    //function createMineSpaces (Somewhat achieved in 'createGameTiles'?)
         tile.classList.add('boom-space');
         tile.innerText = "BOOM";
         totalBoomSpaces++;
+    //make game spaces clickable
         tile.addEventListener('click', handleBoomSpace)
     } else {
         tile.classList.add('open-space');
         totalOpenSpaces++;
+    //make game spaces clickable
         tile.addEventListener('click', handleOpenSpace)
     }
     return tile;
 }
-
-
-//function createMineSpaces (Somewhat achieved in 'createGameTiles'?)
-//randomly placed 
-//up to a certain number (per difficulty)
 
 function handleBoomSpace(event) {
     const boomSpaces = document.querySelectorAll('.boom-space');
@@ -101,20 +97,21 @@ function handleBoomSpace(event) {
 function handleOpenSpace(event) {
     //click on space to 'reveal' 
     //check for open space or bomb space
+    //USE YT VID FOR REFERENCE
     //if open, clear surrounding spaces/list number of mines around 3x3
     //use ternary operator to count bombs
     // totalOpenSpaces.style.backgroundColor = lightgrey;
     const openSpaces = document.querySelectorAll('.open-space');
     event.target.style.backgroundColor = '#c4c4c4';
-
+    //function countBombs()
+    //check for win/loss/in-progress
+    //function isWinner()
+    
 }
 
-// function handleMineSpace() {
+// function countBombs() {
     //else reveal mines upon loss (hopefully)
-    //display a "death" message 
-    //turn start button to restart button
-    // totalBoomSpaces.style.backgroundColor = red;
-// }
+    
 
 
 
